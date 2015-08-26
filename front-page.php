1832138bccdeb2f40409e1e9216b15b9ec680e31
joelworsham <joelworsham@gmail.com>
2015-08-26 12:40:24 -0400

    Make it WERK

--- front-page.php
+++ front-page.php
@@ -16,13 +16,18 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<ul>
+
+			<a href="#" data-reveal-id="gf-ping-form">Open the Modal without sending an ID (just for testing)</a>
+
+			<ul class="user-grid">
 				<li>
 					<span>Ann Arbor</span><br/>
 					<span>Michigan</span><br/>
 					<span>Pets: One cat</span><br/>
 					<span>Capacity: 4 people</span><br/>
-					<a href="#" data-reveal-id="gf-ping-form">THUMB ICON</a>
+
+					<!-- 99 is the user ID, obviously this will change based on the user that this thumb is for -->
+					<a href="#" class="modal-trigger" data-user-id="1" data-reveal-id="gf-ping-form">THUMB ICON</a>
 				</li>
 				<li>
 					<span>Ann Arbor</span><br/>
@@ -30,6 +35,9 @@ get_header();
 					<span>Pets: One cat</span><br/>
 					<span>Capacity: 4 people</span><br/>
 					<span>THUMB ICON</span>
+
+					<!-- 99 is the user ID, obviously this will change based on the user that this thumb is for -->
+					<a href="#" class="modal-trigger" data-user-id="2" data-reveal-id="gf-ping-form">THUMB ICON</a>
 				</li>
 				<li>
 					<span>Ann Arbor</span><br/>
@@ -37,6 +45,9 @@ get_header();
 					<span>Pets: One cat</span><br/>
 					<span>Capacity: 4 people</span><br/>
 					<span>THUMB ICON</span>
+
+					<!-- 99 is the user ID, obviously this will change based on the user that this thumb is for -->
+					<a href="#" class="modal-trigger" data-user-id="3" data-reveal-id="gf-ping-form">THUMB ICON</a>
 				</li>
 				<li>
 					<span>Ann Arbor</span><br/>
@@ -44,6 +55,9 @@ get_header();
 					<span>Pets: One cat</span><br/>
 					<span>Capacity: 4 people</span><br/>
 					<span>THUMB ICON</span>
+
+					<!-- 99 is the user ID, obviously this will change based on the user that this thumb is for -->
+					<a href="#" class="modal-trigger" data-user-id="4" data-reveal-id="gf-ping-form">THUMB ICON</a>
 				</li>
 			</ul>
 		</main><!-- #main -->
@@ -51,6 +65,9 @@ get_header();
 			<h2 id="modalTitle">Awesome. I have it.</h2>
 			<p class="lead">Your couch.  It is mine.</p>
 			<p>I'm a cool paragraph that lives inside of an even cooler modal. Wins!</p>
+
+			<!-- This mimicks the input from the GF form that you will need to find. Note there's no value yet -->
+			<p>The ID you just passed is.... *drumroll* ... <input type="text" class="some-notable-class-or-something" />!!</p>
 			<a class="close-reveal-modal" aria-label="Close">&#215;</a>
 		</div>
 	</div><!-- #primary -->
